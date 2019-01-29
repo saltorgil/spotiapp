d06d5581c4099c92f0247faca99f026da9bec071
Salva Torru <salva339@gmail.com>
2019-01-29 17:03:31 +0000

    Final practica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+import {HttpClientModule} from "@angular/common/http";
+
+// Importar rutas
+import { ROUTES } from './app.routes';
+
+// Services
+import { SpotifyService } from './services/spotify.service';
+
+// Pipes
+import { NoimagePipe } from './pipes/noimage.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
+
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent,
+    NoimagePipe,
+    TarjetasComponent,
+    LoadingComponent,
+    DomseguroPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot( ROUTES, {useHash: true} )
   ],
-  providers: [],
+  providers: [
+    SpotifyService
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

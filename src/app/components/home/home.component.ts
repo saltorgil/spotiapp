f76296fdd55bce7e97b8214ebe39824d0a85ff9e
Salva Torru <salva339@gmail.com>
2020-06-27 13:32:47 +0100

    obtener token auto

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,47 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { SpotifyService } from 'src/app/services/spotify.service';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { SpotifyService } from "src/app/services/spotify.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styles: []
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styles: [],
 })
-
-
 export class HomeComponent implements OnInit {
-
   nuevasCanciones: any[] = [];
 
   loading: boolean;
 
   error: boolean;
 
-  mensajeError : string;
+  mensajeError: string;
 
-  constructor(private spotify : SpotifyService) {
-
+  constructor(private spotify: SpotifyService) {
     this.loading = true;
 
     this.error = false;
 
-    this.spotify.getNewReleases()
-      .subscribe( (data: any) => {
-          this.nuevasCanciones = data;
-          this.loading = false;
-      }, (errorServicio)=>{
+    this.spotify.getNewReleases().subscribe(
+      (data: any) => {
+        this.nuevasCanciones = data;
+        this.loading = false;
+      },
+      (errorServicio) => {
         this.loading = false;
         this.error = true;
-        this.mensajeError = errorServicio.error.error.message
-
-      });
+        this.mensajeError = errorServicio.error.error.message;
+      }
+    );
   }
 
-
-
-  ngOnInit(){
-  }
-
-
-
+  ngOnInit() {}
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,64 +1,75 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { map, switchMap } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { from } from "rxjs";
+
+import { Component, OnInit } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-
-
 export class SpotifyService {
+  token: string;
 
   constructor(private http: HttpClient) {
-    console.log('Servicio Spotify listo')
-   }
+    console.log("Servicio Spotify listo");
+  }
 
+  getToken() {
+    const body = new URLSearchParams();
+    body.set("grant_type", "client_credentials");
+    body.set("client_id", "cc8a795e74564ff08af5bf02a4b3c998");
+    body.set("client_secret", "151ccbc1f40149328d7ecf058794bfee");
 
-   getQuery( query:string)
-   {
-     const url = 'https://api.spotify.com/v1/'+query;
+    const headers = new HttpHeaders({
+      "Content-Type": "application/x-www-form-urlencoded",
+    });
 
-     const headers = new HttpHeaders({
-       'Authorization': 'Bearer  xBQBNhwDoxsx0A50QzOU0Dme2N12ngYEEaHuj46YWPOM8fueJeL2uEmI5n_2unTYjlvjnXFI11fw-xhofGaI'
+    return this.http.post(
+      "https://accounts.spotify.com/api/token",
+      body.toString(),
+      {
+        headers,
+      }
+    );
+  }
 
-     });
+  getQuery(query: string): Observable<Object> {
+    return this.getToken().pipe(
+      switchMap((token) => {
+        const url = "https://api.spotify.com/v1/" + query;
 
-     return this.http.get(url, {headers});
+        console.log("token", token);
 
-   }
+        const headers = new HttpHeaders({
+          Authorization: "Bearer  " + token["access_token"],
+        });
+        return this.http.get(url, { headers });
+      })
+    );
+  }
 
+  getNewReleases() {
+    return this.getQuery("browse/new-releases?limit=20").pipe(
+      map((data) => data["albums"].items)
+    );
+  }
 
-   getNewReleases(){
+  getArtistas(termino: string) {
+    return this.getQuery(
+      "search?q=" + termino + "&type=artist&market=es&limit=15"
+    ).pipe(map((data) => data["artists"].items));
+  }
 
-     return this.getQuery('browse/new-releases?limit=20')
-        .pipe( map( data => data['albums'].items) )
-
-   }
-
-   getArtistas(termino:string){
-
-     return this.getQuery('search?q='+termino+'&type=artist&market=es&limit=15')
-     .pipe( map( data => data['artists'].items))
-
-
-   }
-
-
-   getArtista(id:string){
-
-     return this.getQuery('artists/'+id);
+  getArtista(id: string) {
+    return this.getQuery("artists/" + id);
     // .pipe( map( data => data['artists'].items))
+  }
 
-
-   }
-
-   getTopTracks(id:string){
-
-     return this.getQuery('artists/'+id+'/top-tracks?country=us')
-      .pipe( map( data => data['tracks']))
-
-
-   }
-
-
+  getTopTracks(id: string) {
+    return this.getQuery("artists/" + id + "/top-tracks?country=us").pipe(
+      map((data) => data["tracks"])
+    );
+  }
 }

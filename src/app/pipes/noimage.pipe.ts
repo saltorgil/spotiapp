10659764c5b08135fbdb71f644b30c391693988b
Salva Torru <salva339@gmail.com>
2020-07-18 12:25:41 +0100

    fixed image pipe

--- src/app/pipes/noimage.pipe.ts
+++ src/app/pipes/noimage.pipe.ts
@@ -5,26 +5,8 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class NoimagePipe implements PipeTransform {
   transform(images: any[]): string {
-    /*   if (!images){
-    return 'assets/img/noimage.png'
-  }
-
-  if (images.length > 0)
-  {
-    return images[0].url
-  }
-  else{
-    return 'assets/img/noimage.png'
-  } */
-
-    /*   if (!images){
-    return 'assets/img/noimage.png'
-  }
-  
-  return images.length > 0 ?  images[0].url : 'assets/img/noimage.png'; */
-
-    let image: string = "assets/img/noimage.png";
-    image = images && images.length > 0 && images[0].url;
+    let image =
+      images && images.length > 0 ? images[0].url : "assets/img/noimage.png";
 
     return image;
   }
